65511711106606e38d0bd7edf4034fc094eb2b1a
Sami-Wafula <samiwafula3257@gmail.com>
2022-08-23 17:26:34 +0300

    edit, test_config defined

--- flywhisk/__init__.py
+++ flywhisk/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_app():
+def create_app(test_config=None):
     # app creation and configuration
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
